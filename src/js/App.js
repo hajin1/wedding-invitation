c77f8973046628106025b57f858e666fd4c92b79
hajin <hajin@synapsoft.co.kr>
2022-03-01 10:55:41 +0900

    프로젝트 세팅

---
+++ src/js/App.js
@@ -0,0 +1,6 @@
+import '../scss/style.scss';
+
+export default class App {
+    constructor() {
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const config = {
+        entry: {
+            bundle: "./src/js/index.js",
+        },
+        output: {
+            filename: "[name].js",
+            path: path.resolve("./public"),
+        },
+        devServer: {
+            port: 9000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(scss|css)$/,
+                    use: [
+                        (argv.mode === 'production')
+                            ? MiniCssExtractPlugin.loader
+                            : "style-loader",
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                },
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({ filename: `[name].css` }),
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                filename: '../public/index.html',
+            }),
+        ]
+    };
+    return config;
+}
