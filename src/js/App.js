cf4ec621babcc1b4c374e2e1f2bd68705af29a97
hajin <hajinkim@kangwon.ac.kr>
2022-03-03 01:29:41 +0900

    꽃잎떨어지는 효과, 아래 이동 버튼, 인스타 유도

--- src/js/App.js
+++ src/js/App.js
@@ -40,6 +40,24 @@ export default class App {
             }
         }
 
+        function reveal() {
+            var reveals = document.querySelectorAll(".reveal");
+
+            for (var i = 0; i < reveals.length; i++) {
+                var windowHeight = window.innerHeight;
+                var elementTop = reveals[i].getBoundingClientRect().top;
+                var elementVisible = 5;
+
+                if (elementTop < windowHeight - elementVisible) {
+                    reveals[i].classList.add("active");
+                } else {
+                    reveals[i].classList.remove("active");
+                }
+            }
+        }
+
+        window.addEventListener("scroll", reveal);
+
         window.addEventListener('load', scrollAnimationFunction);
         window.addEventListener('scroll', scrollAnimationFunction);
     }
