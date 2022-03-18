fd3f083196d41796b3760582fa327a2dca6c2372
hajin <hajinkim@kangwon.ac.kr>
2022-03-18 16:12:47 +0900

    image 변경

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import '../scss/style.scss';
 import introImg from '../../images/intro_image.png';
+import storyBackgroundImg from '../../images/story_background.jpeg'
 import Marquee from './Marquee';
 import 'bootstrap';
 
@@ -8,7 +9,8 @@ export default class App {
         // this.init();
         this.bindSmoothEffect();
         this.marquee = new Marquee();
-        this.marquee.renderMarquee();
+
+        this.initImg();
     }
 
     bindSmoothEffect() {
@@ -34,33 +36,12 @@ export default class App {
     }
 
     init() {
-        // Get the modal
-        var modal = document.getElementById("myModal");
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-
-        // When the user clicks on the button, open the modal
-        btn.onclick = function () {
-            modal.style.display = "block";
-        }
-
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function () {
-            modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
     }
 
-
+    initImg() {
+        const img = document.getElementById('story_background')
+        img.src = storyBackgroundImg;
+    }
 }
 
--- src/js/Marquee.js
+++ src/js/Marquee.js
@@ -4,50 +4,107 @@ import img3 from '../../images/marquee_3.jpeg';
 import img4 from '../../images/marquee_4.jpeg';
 import img5 from '../../images/marquee_5.jpeg';
 import img6 from '../../images/marquee_6.jpeg';
+
 import '../scss/style.scss';
-var imageSize = ['center-cropped-small', 'center-cropped-middle', 'center-cropped-big']
-var previous = 4;
+const imageSize = ['center-cropped-small', 'center-cropped-middle', 'center-cropped-big']
+const previous = 4;
 
 export default class Marquee {
     constructor() {
         this.elMarquee = document.getElementsByClassName('marquee')[0];
         this.elContents = this.elMarquee.querySelector('.content');
+        this.elCarousel = this.elMarquee.querySelector('.carousel-inner');
         this.images = [img1, img2, img3, img4, img5, img6];
+        this.init();
+    }
+
+    init() {
+        this.bindEvent();
+        this.renderMarquee();
+        this.renderCarousel();
+    }
+
+    bindEvent() {
+        window.onclick = function (event) {
+            const modal = document.getElementById("picture_modal");
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        }
+
     }
 
     renderMarquee() {
-        this.images.forEach((src) => {
-            const elImage = new Image(src).getElement();
+        this.images.forEach((src, idx) => {
+            const elImage = new Image(idx, src).getElement();
             this.elContents.appendChild(elImage);
         });
     }
+
+    renderCarousel() {
+        this.images.forEach((src, idx) => {
+            const elImage = new CarouselImage(idx, src).getElement();
+            this.elCarousel.appendChild(elImage);
+        })
+    }
+}
+
+class CarouselImage {
+    constructor(idx, src) {
+        this.idx = idx;
+        this.src = src;
+    }
+
+    getElement() {
+        const element = document.createElement('div');
+        const img = document.createElement('img');
+        img.setAttribute('src', this.src);
+        element.append(img);
+        element.classList.add('carousel-item');
+        element.setAttribute('data-id', this.idx);
+        return element;
+    }
+
+
 }
 
 class Image {
-    constructor(src) {
+    constructor(idx, src) {
+        this.idx = idx;
         this.src = src;
     }
 
     onClick() {
-        console.log('click image');
+        const modal = document.getElementById("picture_modal");
+        modal.style.display = "block";
+
+        // const carouselImage = document.querySelector(`.carousel-item`);
+        console.log(this.idx);
+        const carouselImage = document.querySelector(`.carousel-item[data-id="${this.idx}"]`);
+        const items = document.querySelectorAll('.carousel-item');
+        items.forEach((img) => {
+            img.classList.remove('active');
+        });
+
+        console.log(carouselImage);
+        carouselImage.classList.add('active');
     }
-
-
 
     getElement() {
         const img = document.createElement('img');
         img.setAttribute('src', this.src);
 
-        var random = Math.floor(Math.random() * 3);
+        const random = Math.floor(Math.random() * 3);
 
         if (previous === random)
             random = (random + 1) % 3;
 
         img.classList.add(imageSize[random]);
         img.classList.add('modalBtn');
-        img.addEventListener('click', this.onClick);
+        img.setAttribute('data-id', this.idx);
+        img.addEventListener('click', this.onClick.bind(this));
 
-        previous = random;
+        // previous = random;
         return img;
     }
 }
