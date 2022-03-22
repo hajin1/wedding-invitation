028bb20c867a02d810ddece213677e81ba01ed55
hajin <hajinkim@kangwon.ac.kr>
2022-03-22 22:46:05 +0900

    기타

--- src/js/App.js
+++ src/js/App.js
@@ -3,14 +3,18 @@ import introImg from '../../images/intro_image.png';
 import storyBackgroundImg from '../../images/story_background.jpeg'
 import Marquee from './Marquee';
 import 'bootstrap';
+import { doc } from 'prettier';
+
 
 export default class App {
     constructor() {
-        // this.init();
+
         this.bindSmoothEffect();
         this.marquee = new Marquee();
 
         this.initImg();
+
+        this.init();
     }
 
     bindSmoothEffect() {
@@ -36,6 +40,50 @@ export default class App {
     }
 
     init() {
+        const section = document.getElementById("canvas_section"); //document.documentElement
+        console.log(section)
+
+        const canvas = document.getElementById("hero-lightpass");
+        const context = canvas.getContext("2d");
+
+        const frameCount = 148;
+        const currentFrame = index => (
+            `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
+        )
+
+        const preloadImages = () => {
+            for (let i = 1; i < frameCount; i++) {
+                const img = new Image();
+                img.src = currentFrame(i);
+            }
+        };
+
+        const img = new Image()
+        img.src = currentFrame(1);
+        canvas.width = 1158;
+        canvas.height = 770;
+        img.onload = function () {
+            context.drawImage(img, 0, 0);
+        }
+
+        const updateImage = index => {
+            img.src = currentFrame(index);
+            context.drawImage(img, 0, 0);
+        }
+
+        window.addEventListener('scroll', () => {
+            const scrollFraction = section.scrollTop / (section.scrollHeight - window.innerHeight);
+            console.log(scrollFraction)
+
+            const frameIndex = Math.min(
+                frameCount - 1,
+                Math.ceil(scrollFraction * frameCount)
+            );
+
+            requestAnimationFrame(() => updateImage(frameIndex + 1))
+        });
+
+        preloadImages()
 
     }
 
