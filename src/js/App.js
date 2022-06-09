bbbb68a0024d939fbc9c5eb6242437d63f66f785
hajin <hajinkim@kangwon.ac.kr>
2022-06-10 00:14:13 +0900

    달력 추가, 오시는길 추가

--- src/js/App.js
+++ src/js/App.js
@@ -4,6 +4,7 @@ import storyImg1Src from '../../images/story1.jpeg';
 import storyImg2Src from '../../images/story2.jpeg';
 import storyImg3Src from '../../images/story3.jpeg';
 import storyImg4Src from '../../images/story4.jpeg';
+import googleCalendarIcon from '../../images/google-calendar-icon.png';
 
 export default class App {
     constructor() {
@@ -23,11 +24,25 @@ export default class App {
         const storyImg4 = document.getElementById('story-img4');
         storyImg4.src = storyImg4Src;
 
+        this.createGoogleCalendarButton();
         // const sections = document.getElementsByClassName('section');
         // console.log(window.innerHeight);
         // sections[0].style.height = `${window.innerHeight}px`;
         // sections[1].style.height = `${window.innerHeight}px`;
 
+    }
+
+    createGoogleCalendarButton() {
+        const buttonElm = document.getElementById('google-calendar-button');
+        buttonElm.addEventListener('click', () => {
+            const url = "https://calendar.google.com/calendar/render?action=TEMPLATE&text=성수와 하진이의 결혼식&dates=20221002T123000/20221002T143000&details=❤︎&location=엘블레스, 대한민국 서울특별시 서초구 강남대로";
+            window.open(url);
+        });
+        const iconElm = document.createElement('img');
+        iconElm.src = googleCalendarIcon;
+        console.log(iconElm);
+        console.log(buttonElm);
+        buttonElm.append(iconElm);
     }
 
 }
