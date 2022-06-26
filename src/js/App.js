f7b743153241b98dbed03e7d0721515a75a75075
hajin <hajinkim@kangwon.ac.kr>
2022-06-26 22:28:17 +0900

    마음전하실곳 추가

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,9 @@ import googleCalendarIcon from '../../resources/google-calendar-icon.png';
 import kakaoMapIcon from '../../resources/kakaomap_icon.png';
 import naverMapIcon from '../../resources/navermap_icon.png';
 import copyLinkIcon from '../../resources/copylink_icon.png';
-import sendIcon from '../../resources/send.png';
+
+const ACCOUNT_NUM1 = '110334785476';
+const ACCOUNT_NUM2 = '110325340667';
 
 export default class App {
     constructor() {
@@ -14,7 +16,35 @@ export default class App {
         this.createGoogleCalendarButton();
         this.bindMapButton();
         this.bindInstaButton();
-        // this.bindInviteVideoLink();
+        this.bindAccountToggle();
+    }
+
+    bindAccountToggle() {
+        const titleElm1 = document.getElementById('account-title1');
+        titleElm1.addEventListener('click', () => {
+            const accountElm = document.getElementById('account-1');
+            accountElm.classList.contains('hide') ? accountElm.classList.remove('hide') : accountElm.classList.add('hide');
+        });
+
+        const titleElm2 = document.getElementById('account-title2');
+        titleElm2.addEventListener('click', () => {
+            const accountElm = document.getElementById('account-2');
+            accountElm.classList.contains('hide') ? accountElm.classList.remove('hide') : accountElm.classList.add('hide');
+        });
+
+        const copyButton1 = document.getElementById('account-copy-button1');
+        copyButton1.addEventListener('click', () => {
+            navigator.clipboard.writeText(ACCOUNT_NUM1).then(() => {
+                window.alert(`계좌번호(${ACCOUNT_NUM1})가 복사되었습니다.\n필요한 곳에 붙여넣기 하세요`);
+            });
+        });
+
+        const copyButton2 = document.getElementById('account-copy-button2');
+        copyButton2.addEventListener('click', () => {
+            navigator.clipboard.writeText(ACCOUNT_NUM2).then(() => {
+                window.alert(`계좌번호(${ACCOUNT_NUM2})가 복사되었습니다.\n필요한 곳에 붙여넣기 하세요`);
+            });
+        });
     }
 
     bindInstaButton() {
@@ -22,10 +52,6 @@ export default class App {
         button.addEventListener('click', () => {
             window.open('https://www.instagram.com/jin_hiking_s2/');
         });
-
-        const sendIconElm = document.createElement('img');
-        sendIconElm.src = sendIcon;
-        button.append(sendIconElm);
     }
 
     bindMapButton() {
@@ -41,8 +67,9 @@ export default class App {
 
         const copyButton = document.getElementById('copy-link-button');
         copyButton.addEventListener('click', () => {
-            navigator.clipboard.writeText('서울 서초구 강남대로 213 스포타임 지하1층 엘블레스');
-            window.alert('주소가 복사되었습니다!');
+            navigator.clipboard.writeText('서울 서초구 강남대로 213 스포타임 지하1층 엘블레스').then(() => {
+                window.alert('주소가 복사되었습니다!');
+            });
         });
 
         const kakaoIconElm = document.createElement('img');
@@ -68,13 +95,6 @@ export default class App {
         iconElm.src = googleCalendarIcon;
         buttonElm.prepend(iconElm);
     }
-
-    // bindInviteVideoLink() {
-    //     const elm = document.getElementById('invite-video');
-    //     elm.addEventListener('click', () => {
-    //         window.open('https://youtu.be/WUaBlwl4X3M');
-    //     })
-    // }
 
 }
 
