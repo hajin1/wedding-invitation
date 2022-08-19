5cd8868692d975542dfac703becdf8d29d7a5278
hajin <hajinkim@kangwon.ac.kr>
2022-08-19 18:01:05 +0900

    ClipboardJS 적용, 부모님계좌 추가

--- src/js/App.js
+++ src/js/App.js
@@ -4,124 +4,136 @@ import kakaoMapIcon from '../../resources/img/kakaomap_icon.png';
 import naverMapIcon from '../../resources/img/navermap_icon.png';
 import copyLinkIcon from '../../resources/img/copylink_icon.png';
 import unfoldIcon from '../../resources/img/unfold.png';
-
-const ACCOUNT_NUM1 = '110334785476';
-const ACCOUNT_NUM2 = '110325340667';
+import { ACCOUNT_INFO } from './const';
 
 export default class App {
-    constructor() {
-        this.init();
-    }
+	constructor() {
+		this.init();
+	}
 
-    init() {
-        this.createGoogleCalendarButton();
-        this.bindMapButton();
-        this.bindInstaButton();
-        this.bindAccountToggle();
+	init() {
+		this.renderAccountInfo();
+		this.createGoogleCalendarButton();
+		this.bindMapButton();
+		this.bindInstaButton();
 
-        const coverElm = document.getElementById('cover');
-        coverElm.addEventListener('animationend', () => {
-            coverElm.classList.add('hidden');
+		const coverElm = document.getElementById('cover');
+		coverElm.addEventListener('animationend', () => {
+			coverElm.classList.add('hidden');
 
-            const contentLayerElm = document.getElementById('content-layer');
-            if (contentLayerElm.classList.contains('hidden')) {
-                contentLayerElm.classList.remove('hidden');
-                contentLayerElm.classList.add('show');
-            }
-        });
-    }
+			const contentLayerElm = document.getElementById('content-layer');
+			if (contentLayerElm.classList.contains('hidden')) {
+				contentLayerElm.classList.remove('hidden');
+				contentLayerElm.classList.add('show');
+			}
+		});
+	}
 
-    bindAccountToggle() {
-        const titleElm1 = document.getElementById('account-title1');
-        titleElm1.addEventListener('click', () => {
-            const accountElm = document.getElementById('account-1');
-            if (accountElm.classList.contains('hide')) {
-                accountElm.classList.remove('hide');
-                titleElm1.querySelector('img').classList.add('rotate');
-            } else {
-                accountElm.classList.add('hide');
-                titleElm1.querySelector('img').classList.remove('rotate');
-            }
-        });
+	createAccountItem(account) {
+		const elm = document.createElement('div');
+		elm.classList.add('account-item');
+		const bankElm = document.createElement('div');
+		bankElm.innerHTML = `${account.bank} ${account.holder}<br />${account.number}`;
+		const buttonElm = document.createElement('button');
+		buttonElm.setAttribute('data-clipboard-text', `${account.bank} ${account.number.replaceAll('-', '')}`);
 
-        const titleElm2 = document.getElementById('account-title2');
-        titleElm2.addEventListener('click', () => {
-            const accountElm = document.getElementById('account-2');
-            if (accountElm.classList.contains('hide')) {
-                accountElm.classList.remove('hide');
-                titleElm2.querySelector('img').classList.add('rotate');
-            } else {
-                accountElm.classList.add('hide');
-                titleElm2.querySelector('img').classList.remove('rotate');
-            }
-        });
+		buttonElm.textContent = '복사하기';
+		buttonElm.classList.add('copy-btn');
 
-        titleElm1.querySelector('img').src = unfoldIcon;
-        titleElm2.querySelector('img').src = unfoldIcon;
+		elm.append(bankElm);
+		elm.append(buttonElm);
 
-        const copyButton1 = document.getElementById('account-copy-button1');
-        copyButton1.addEventListener('click', () => {
-            navigator.clipboard.writeText(ACCOUNT_NUM1).then(() => {
-                window.alert(`계좌번호(${ACCOUNT_NUM1})가 복사되었습니다.\n필요한 곳에 붙여넣기 하세요`);
-            });
-        });
+		return elm;
+	}
 
-        const copyButton2 = document.getElementById('account-copy-button2');
-        copyButton2.addEventListener('click', () => {
-            navigator.clipboard.writeText(ACCOUNT_NUM2).then(() => {
-                window.alert(`계좌번호(${ACCOUNT_NUM2})가 복사되었습니다.\n필요한 곳에 붙여넣기 하세요`);
-            });
-        });
-    }
+	renderAccountInfo() {
+		const groomListElm = document.getElementById('groom-account-list');
+		ACCOUNT_INFO.groom.map(account => {
+			const elm = this.createAccountItem(account);
+			groomListElm.append(elm);
+		});
 
-    bindInstaButton() {
-        const button = document.getElementById('insta-button');
-        button.addEventListener('click', () => {
-            window.open('https://www.instagram.com/jin_hiking_s2/');
-        });
-    }
+		const brideListElm = document.getElementById('bride-account-list');
+		ACCOUNT_INFO.bride.map(account => {
+			const elm = this.createAccountItem(account);
+			brideListElm.append(elm);
+		});
 
-    bindMapButton() {
-        const naverButton = document.getElementById('naver-map-button');
-        naverButton.addEventListener('click', () => {
-            window.open('https://map.naver.com/v5/directions/-/14141468.151074853,4506638.338950755,엘블레스,37688101,PLACE_POI/-/transit?c=14141002.3740654,4506638.3529793,15,0,0,0,dh');
-        });
+		const clipboard = new ClipboardJS('.copy-btn');
+		clipboard.on('success', e => {
+			window.alert(`계좌번호(${e.text})가 복사되었습니다. 필요한 곳에 붙여넣기 하세요`);
+		});
 
-        const kakaoButton = document.getElementById('kakao-map-button');
-        kakaoButton.addEventListener('click', () => {
-            window.open('https://map.kakao.com/link/to/엘블레스,37.48281604699608,127.03482927733398');
-        });
+		this.bindAccountToggle();
+	}
 
-        const copyButton = document.getElementById('copy-link-button');
-        copyButton.addEventListener('click', () => {
-            navigator.clipboard.writeText('서울 서초구 강남대로 213 스포타임 지하1층 엘블레스').then(() => {
-                window.alert('주소가 복사되었습니다!');
-            });
-        });
+	bindAccountToggle() {
+		const elms = document.getElementsByClassName('account-title');
 
-        const kakaoIconElm = document.createElement('img');
-        kakaoIconElm.src = kakaoMapIcon;
-        kakaoButton.prepend(kakaoIconElm);
+		Array.from(elms).map(elm => {
+			elm.querySelector('img').src = unfoldIcon;
 
-        const naverIconElm = document.createElement('img');
-        naverIconElm.src = naverMapIcon;
-        naverButton.prepend(naverIconElm);
+			const listElm = elm.parentElement.querySelector('.account-list');
+			listElm.setAttribute('style', `height: ${listElm.childElementCount * 50}px`);
+			elm.addEventListener('click', () => {
+				if (listElm.classList.contains('hide')) {
+					listElm.classList.remove('hide');
+					elm.querySelector('img').classList.add('rotate');
+				} else {
+					listElm.classList.add('hide');
+					elm.querySelector('img').classList.remove('rotate');
+				}
+			});
+		});
+	}
 
-        const copyIconElm = document.createElement('img');
-        copyIconElm.src = copyLinkIcon;
-        copyButton.prepend(copyIconElm);
-    }
+	bindInstaButton() {
+		const button = document.getElementById('insta-button');
+		button.addEventListener('click', () => {
+			window.open('https://www.instagram.com/jin_hiking_s2/');
+		});
+	}
 
-    createGoogleCalendarButton() {
-        const buttonElm = document.getElementById('google-calendar-button');
-        buttonElm.addEventListener('click', () => {
-            const url = "https://calendar.google.com/calendar/render?action=TEMPLATE&text=성수와 하진이의 결혼식&dates=20221002T123000/20221002T143000&details=❤︎&location=엘블레스, 대한민국 서울특별시 서초구 강남대로";
-            window.open(url);
-        });
-        const iconElm = document.createElement('img');
-        iconElm.src = googleCalendarIcon;
-        buttonElm.prepend(iconElm);
-    }
+	bindMapButton() {
+		const naverButton = document.getElementById('naver-map-button');
+		naverButton.addEventListener('click', () => {
+			window.open('https://map.naver.com/v5/directions/-/14141468.151074853,4506638.338950755,엘블레스,37688101,PLACE_POI/-/transit?c=14141002.3740654,4506638.3529793,15,0,0,0,dh');
+		});
 
+		const kakaoButton = document.getElementById('kakao-map-button');
+		kakaoButton.addEventListener('click', () => {
+			window.open('https://map.kakao.com/link/to/엘블레스,37.48281604699608,127.03482927733398');
+		});
+
+		const copyButton = document.getElementById('copy-link-button');
+		copyButton.addEventListener('click', () => {
+			navigator.clipboard.writeText('서울 서초구 강남대로 213 스포타임 지하1층 엘블레스').then(() => {
+				window.alert('주소가 복사되었습니다!');
+			});
+		});
+
+		const kakaoIconElm = document.createElement('img');
+		kakaoIconElm.src = kakaoMapIcon;
+		kakaoButton.prepend(kakaoIconElm);
+
+		const naverIconElm = document.createElement('img');
+		naverIconElm.src = naverMapIcon;
+		naverButton.prepend(naverIconElm);
+
+		const copyIconElm = document.createElement('img');
+		copyIconElm.src = copyLinkIcon;
+		copyButton.prepend(copyIconElm);
+	}
+
+	createGoogleCalendarButton() {
+		const buttonElm = document.getElementById('google-calendar-button');
+		buttonElm.addEventListener('click', () => {
+			const url =
+				'https://calendar.google.com/calendar/render?action=TEMPLATE&text=성수와 하진이의 결혼식&dates=20221002T123000/20221002T143000&details=❤︎&location=엘블레스, 대한민국 서울특별시 서초구 강남대로';
+			window.open(url);
+		});
+		const iconElm = document.createElement('img');
+		iconElm.src = googleCalendarIcon;
+		buttonElm.prepend(iconElm);
+	}
 }
-
---
+++ src/js/const.js
@@ -0,0 +1,32 @@
+
+export const ACCOUNT_INFO = {
+    groom: [
+        {
+            bank: '국민은행',
+            holder: '이영철',
+            number: '624401-04-163092'
+        },
+        {
+            bank: '농협',
+            holder: '김봉옥',
+            number: '893-02-242241'
+        },
+        {
+            bank: '신한은행',
+            holder: '이성수',
+            number: '110-334-785476'
+        },
+    ],
+    bride: [
+        {
+            bank: '농협',
+            holder: '김귀원',
+            number: '221029-52-058167'
+        },
+        {
+            bank: '신한은행',
+            holder: '김하진',
+            number: '110-325-340667'
+        },
+    ]
+};
